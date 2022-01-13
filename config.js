374dbf08eb239ef77a8bfa6aa7df76ab49ab46d4
starlord_ag <73152473+starlordag@users.noreply.github.com>
2022-01-12 17:48:11 -0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig={
+    apiKey: "AIzaSyArQ3ceF0kbdMUsxYSh5MPt73gTzmMXoo8",
+    authDomain: "storytelling-app-42e04.firebaseapp.com",
+    projectId: "storytelling-app-42e04",
+    storageBucket: "storytelling-app-42e04.appspot.com",
+    messagingSenderId: "245740775795",
+    appId: "1:245740775795:web:ecb8d87272fcc479613d06"
+  };
